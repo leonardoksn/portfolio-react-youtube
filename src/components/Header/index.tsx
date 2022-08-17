4760dab6b0ae0cf6ca090c03ff3db9b235289527
leonardo.nunes <leonardo.ksn@gmail.com>
2022-08-17 09:00:46 -0300

    Main header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,22 @@
 import React from 'react';
+import { CTA } from './CTA';
+import ME from '../../assets/me.png';
 import './header.index.css';
-const Header = () => <div>Header</div>;
+const Header = () =>
+    <header>
+        <div className='container header__container'>
+            <h5>Hello I'm</h5>
+            <h1>Leonardo Nunes</h1>
+            <h5 className='text-light'>Fullstack Developer</h5>
+            <CTA />
+
+            <div className='me'>
+                <img src={ME} alt='me' />
+            </div>
+
+            <a href='#contact' className='scroll__down'>Scroll Down</a>
+        </div>
+
+    </header>;
 
 export { Header };
